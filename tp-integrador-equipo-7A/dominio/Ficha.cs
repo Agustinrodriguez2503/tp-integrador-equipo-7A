72c1ebfe3caeebb0c4e388c62177476ed67ec9af
nahuelpmartinez <nahuepmartinez@gmail.com>
2025-06-04 19:13:47 -0300

    clases de dominio turno, veterinario y ficha

---
+++ tp-integrador-equipo-7A/dominio/Ficha.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Ficha
+    {
+        public int IdFicha { get; set; }
+        public int IdMascota { get; set; }
+        public int IdVeterinario { get; set; }
+        public string Diagnostico { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Activo { get; set; }
+    }
+}
---
+++ tp-integrador-equipo-7A/dominio/Turno.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Turno
+    {
+        public int IdTurno { get; set; }
+        public int IdDueño { get; set; }
+        public int IdVeterinario { get; set; }
+        public DateTime Fecha { get; set; }
+        public bool Activo { get; set; }
+    }
+}
